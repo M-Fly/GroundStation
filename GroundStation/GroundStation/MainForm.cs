a49c0d7df35ae1cda8964ea4a39d522a0b40431b
spareelectronics <aullah@umich.edu>
2016-11-28 19:00:36 -0500

    File Name

--- GroundStation/GroundStation/MainForm.cs
+++ GroundStation/GroundStation/MainForm.cs
@@ -130,6 +130,10 @@ namespace GroundStation
                 Console.WriteLine("Error in reading InputString: YOU MESSED UP!");
             }
 
+            // Saving Data to File
+
+            string name = "M-Fly Telemtry" + DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss");
+
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
