60a6f249f5dd0881de6407d1e942210a14fe266b
CleverFool <liuchun@umich.edu>
2016-12-05 19:53:41 -0500

    Prediction
    Adding my prediction algorithm in the form of comments

--- GroundStation/GroundStation/DropPrediction/PredictionAlgorithmEuler.cs
+++ GroundStation/GroundStation/DropPrediction/PredictionAlgorithmEuler.cs
@@ -55,6 +55,55 @@
 
             // Return the final position vector of the payload
             return posOut;
+            /*
+            % find final x, y, z positions given initial positions and initial
+            % velocities and omega = k / m where k is CdA(rho)
+            % things to do when converting to c#: make this a function
+            % function predictionAlgorithm taking in [x0, y0, z0, v_x0, v_y0]
+            % convert everything into ft i guess
+            %function[percentSuccess, percentError] = predictionAlgorithm(x_f, y_f)
+            omega = rho*payload_area*payload_cdrag/payload_mass;
+            **replace thing in brackets with newton's method to find the final time
+            {syms t
+            fun = @(t) z0 + (g/(omega^2)) - (g/(omega^2))*exp(omega.* t) + (g/omega)*t;
+            tf = fzero(fun, [0,100]);
+            t = 0:.1:tf;}
+            **
+            for (int t = 0; t <= 10; t = t + .1)
+                {
+                z = z0 + (g/(omega^2)) - (g/(omega^2))*exp(omega * t) + (g/omega)*t;
+                if (z = 0) 
+                    {
+                    tf = t;
+                    }
+                }
+            double[] time;        
+            **
+            x = x0 + v_x0/omega - (v_x0/omega)*exp(-omega.* time);
+            y = y0 + v_y0/omega - (v_y0/omega)*exp(-omega.* time);
+            z = z0 + (g/(omega^2)) - (g/(omega^2))*exp(omega.* time) + (g/omega)*time;
+            plot3(x, y, z)
+            hold on
+            x_f = 130;
+            y_f = 130;
+            r_circle = 10;
+            t_circle = 0:.1:2*pi;
+            x_circle = r_circle* cos(t_circle);
+            y_circle = r_circle* sin(t_circle);
+            plot(x_circle+x_f, y_circle+y_f)
+            if (x(end) >= x_circle(1))&& (x(end) <= x_circle(end))
+            if (y(end) >= y_circle(0))&& (y(end) <= y_circle(end))
+            success = 'The payload will land in drop zone'
+            end
+            else
+            success = 'Will not land in drop zone'
+            end
+            %matter of irritation is that fzero is a pretty bad approximation from 
+            %what it seems
+            %longitude and latitude 
+            %plan is if it's possible to get %success from success of trials
+            */
+
         }
     }
 }
