57e7df336f7deb545d1dc8fe9787a44c9c5451b8
Sach Vaidya <sach915@gmail.com>
2017-02-06 17:02:17 -0800

    Created User Control for Wind Parameters (Speed and Direction) and get functions for the two variables

---
+++ GroundStation/GroundStation/Panels/WindParameters.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GroundStation.Panels
+{
+    partial class WindParameters
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WindParameters_Speed = new System.Windows.Forms.Label();
+            this.WindParameters_Direction = new System.Windows.Forms.Label();
+            this.WindParameters_SpeedInput = new System.Windows.Forms.TextBox();
+            this.WindParameters_DirectionInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // WindParameters_Speed
+            // 
+            this.WindParameters_Speed.AutoSize = true;
+            this.WindParameters_Speed.Location = new System.Drawing.Point(0, 0);
+            this.WindParameters_Speed.Name = "WindParameters_Speed";
+            this.WindParameters_Speed.Size = new System.Drawing.Size(183, 25);
+            this.WindParameters_Speed.TabIndex = 0;
+            this.WindParameters_Speed.Text = "Wind Speed (m/s)";
+            // 
+            // WindParameters_Direction
+            // 
+            this.WindParameters_Direction.AutoSize = true;
+            this.WindParameters_Direction.Location = new System.Drawing.Point(0, 62);
+            this.WindParameters_Direction.Name = "WindParameters_Direction";
+            this.WindParameters_Direction.Size = new System.Drawing.Size(253, 25);
+            this.WindParameters_Direction.TabIndex = 1;
+            this.WindParameters_Direction.Text = "Wind Direction (Degrees)";
+            // 
+            // WindParameters_SpeedInput
+            // 
+            this.WindParameters_SpeedInput.Location = new System.Drawing.Point(46, 28);
+            this.WindParameters_SpeedInput.Name = "WindParameters_SpeedInput";
+            this.WindParameters_SpeedInput.Size = new System.Drawing.Size(100, 31);
+            this.WindParameters_SpeedInput.TabIndex = 2;
+            // 
+            // WindParameters_DirectionInput
+            // 
+            this.WindParameters_DirectionInput.Location = new System.Drawing.Point(46, 90);
+            this.WindParameters_DirectionInput.Name = "WindParameters_DirectionInput";
+            this.WindParameters_DirectionInput.Size = new System.Drawing.Size(100, 31);
+            this.WindParameters_DirectionInput.TabIndex = 3;
+            // 
+            // WindParameters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.WindParameters_DirectionInput);
+            this.Controls.Add(this.WindParameters_SpeedInput);
+            this.Controls.Add(this.WindParameters_Direction);
+            this.Controls.Add(this.WindParameters_Speed);
+            this.Name = "WindParameters";
+            this.Size = new System.Drawing.Size(282, 156);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label WindParameters_Speed;
+        private System.Windows.Forms.Label WindParameters_Direction;
+        private System.Windows.Forms.TextBox WindParameters_SpeedInput;
+        private System.Windows.Forms.TextBox WindParameters_DirectionInput;
+    }
+}
---
+++ GroundStation/GroundStation/Panels/WindParameters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GroundStation.Panels
+{
+    public partial class WindParameters : UserControl
+    {
+        public WindParameters()
+        {
+            InitializeComponent();
+        }
+
+        // Returns the Wind Speed that was entered into the textbox. Units are (m/s)
+        public double getWindSpeed()
+        {
+            return Convert.ToDouble(WindParameters_SpeedInput.Text);
+        }
+        
+        // Returns the Wind Direction that was entered into the textbox. Units are (degrees)
+        public double getWindDirection()
+        {
+            return Convert.ToDouble(WindParameters_DirectionInput.Text);
+        }
+    }
+}
